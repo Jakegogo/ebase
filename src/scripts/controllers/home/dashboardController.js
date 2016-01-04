c5cc94fa3db23cc6d4f0b0b81765f359294b95b8
Jake <335923591@qq.com>
2016-01-04 23:44:47 +0800

    项目基础文件

--- src/scripts/controllers/home/dashboardController.js
+++ src/scripts/controllers/home/dashboardController.js
@@ -1,6 +1,4 @@
 angular.module('eBaseFront').controller('DashboardController', function($rootScope, $scope, $http, $timeout) {
     // set sidebar closed and body solid layout mode
-    $rootScope.settings.layout.pageContentWhite = true;
-    $rootScope.settings.layout.pageBodySolid = false;
-    $rootScope.settings.layout.pageSidebarClosed = false;
+
 });
--- src/scripts/controllers/test/test.js
+++ src/scripts/controllers/test/test.js
@@ -1,7 +1,4 @@
 angular.module('eBaseFront').controller('TestController', function($rootScope, $scope, $http, $timeout) {
     // set sidebar closed and body solid layout mode
-    $rootScope.settings.layout.pageContentWhite = true;
-    $rootScope.settings.layout.pageBodySolid = false;
-    $rootScope.settings.layout.pageSidebarClosed = false;
     $scope.aa = 3;
 });
