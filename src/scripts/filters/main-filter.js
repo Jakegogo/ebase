6e8670044d54ca07cad7c59bb3ce837ed556f074
Jake <335923591@qq.com>
2016-01-24 21:15:45 +0800

    项目基础文件

--- src/scripts/filters/main-filter.js
+++ src/scripts/filters/main-filter.js
@@ -1,10 +1,12 @@
 /**
  * Created by Jake on 1/8 0008.
  */
-/**
- * 国际化翻译过滤器
- */
 define(['app'], function (app) {
+
+    /**
+     * 国际化过滤器
+     * {{'默认值'|T:'国际化ID':'参数(支持json object)'}}
+     */
     app.filter("T", ['$translate', '$parse', function ($translate, $parse) {
         return function (defaultVal, key, paramObj) {
             if (!angular.isObject(paramObj)) {
@@ -20,4 +22,5 @@ define(['app'], function (app) {
             return defaultVal;
         };
     }]);
+
 });
